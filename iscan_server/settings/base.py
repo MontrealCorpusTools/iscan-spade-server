8a46a7bdb0f9fed56d9f6b6624108528b92b5acc
Michael Goodale <michaelericgoodale@gmail.com>
2019-05-22 14:08:12 -0400

    Fixed installation issues and new CORS update

--- iscan_server/settings/base.py
+++ iscan_server/settings/base.py
@@ -176,8 +176,8 @@ CORS_ALLOW_CREDENTIALS = True
 
 #CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
-   'localhost:8080',
-   '127.0.0.1:8080'
+    'http://localhost:8080',
+    'http://127.0.0.1:8080'
 )
 
 TEMPLATES = [
